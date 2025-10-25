b9e468107d4fb5ffdda9fd213c2d19e9b7ef7a65
BARNALI SING <singbarnali9@gmail.com>
2025-10-25 18:27:59 +0530

    Add files via upload

---
+++ ass1_task1.py
@@ -0,0 +1,17 @@
+"""taking two numbers as input from user"""
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
+
+
+"""performing operations"""
+Addition=first_num+second_num
+Subtraction=first_num-second_num
+Multiplication=first_num*second_num
+Division=float(first_num/second_num)
+
+
+"""display the results"""
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ", Division)
---
+++ ass1_task2.py
@@ -0,0 +1,9 @@
+"""taking input from user"""
+first_name=input("Enter the first name: ")
+last_name=input("Enter the last name: ")
+
+"""creating full name"""
+full_name=first_name+" "+last_name
+
+"""display the greet message"""
+print("Hello," + " " + full_name + "! Welcome to the Python program")
